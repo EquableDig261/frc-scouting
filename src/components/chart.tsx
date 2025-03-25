195a46a781079c615218dac4600527fec7457a80
EquableDig261 <111559195+EquableDig261@users.noreply.github.com>
2025-03-25 14:06:38 +1100

    line

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis, Legend } from "recharts";
+import { Line, CartesianGrid, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis, Legend } from "recharts";
 
 // Use the provided MatchResult type
 interface MatchResult {
@@ -20,27 +20,30 @@ export default function ClusteredBarChart({ data, teams }: ClusteredBarChartProp
   return (
     <div className="w-full h-64">
       <ResponsiveContainer width="100%" height="100%">
-        <BarChart data={data}>
+        <LineChart data={data}>
           <XAxis dataKey="name" className="text-sm" />
           <YAxis className="text-sm" />
           <Tooltip />
           <Legend />
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
           
           {/* Render each bar only if the category exists */}
           {teams.map((team, index) => {
             const categoryKey = `category${String.fromCharCode(65 + index)}`; // 'A', 'B', 'C', 'D' for team categories
             return (
+              
               data.some((item) => item[categoryKey as keyof MatchResult] !== undefined) && (
-                <Bar
+                <Line
                   key={team}
+                  
                   dataKey={categoryKey as keyof MatchResult} // Use the key as part of dataKey
-                  fill={index % 4 === 0 ? "#3b82f6" : (index % 4 === 1 ? "#ef4444": (index % 4 === 2 ? "#fced23" : "#07b151"))} // Alternate colors for different teams
+                  stroke={index % 4 === 0 ? "#3b82f6" : (index % 4 === 1 ? "#ef4444": (index % 4 === 2 ? "#fced23" : "#07b151"))} // Alternate colors for different teams
                   name={`Team ${team}`}
                 />
               )
             );
           })}
-        </BarChart>
+        </LineChart>
       </ResponsiveContainer>
     </div>
   );
